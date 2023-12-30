c0ae98f730495414a71dde2b47283157d4966b52
Alex Nelson <pqnelson@gmail.com>
2023-12-30 10:33:22 -0800

    Major refactor to imitate Stacks project 'big file' design

--- zk.el
+++ zk.el
@@ -1,3 +1,4 @@
+;; 11
 (require 'calc-bin)
 
 (defvar zk-dir "~/src/tex-zk")
@@ -16,12 +17,55 @@
 
 (defvar zk-recent-file nil)
 
+;; (make-hash-table)
+;; (puthash key value table)
+
+(defun get-nth-line (number)
+  "Get line with NUMBER"
+  (save-restriction
+    (widen)
+    (save-excursion
+      (goto-line number)
+      (buffer-substring-no-properties (line-beginning-position)
+                                      (line-end-position)))))
+
+(defun zk-read-first-line-of-buffer ()
+  (get-nth-line 1))
+
+(defun zk-get-node-number ()
+  (let ((line (zk-read-first-line-of-buffer)))
+    (string-to-number (substring line (min 1 (length line))))))
+
+(defun zk-update-header (n)
+  (interactive)
+  (save-restriction
+    (widen)
+    (save-excursion
+      (goto-line 1)
+      (delete-region (point) (line-end-position))
+      (insert (format "%% %s" (number-to-string n))))))
+
 (defun pad-zeroes (f n)
   (format "%s%s" (make-string (- n (length f)) ?0) f))
 
+(defun zk-generate-tag-name ()
+  (let ((number (zk-get-node-number)))
+    (format "%s-%s"
+            (file-name-base buffer-file-name)
+            (let ((calc-number-radix 36))
+              (pad-zeroes (math-format-radix number) 4)))))
+
+(defun zk-update-number ()
+  (interactive)
+  (zk-update-header (1+ (zk-get-node-number))))
+
+(defun zk-rm-latex-automode ()
+  (when (boundp 'auto-insert-alist)
+    (assq-delete-all 'latex auto-insert-alist)))
+
 (defun zk-new-node ()
   (interactive)
-  (assq-delete-all 'latex auto-insert-alist)
+  (zk-rm-latex-automode)
   (let ((choice (completing-read
                  (format "ZK namespace [%s]: "
                          zk-namespace-default)
@@ -39,7 +83,7 @@
       (setq zk-recent-file (format "%s/%s.tex"
                                    (get-zk-tex-dir-path)
                                    ns))
-      (assq-delete-all 'latex auto-insert-alist)
+      (zk-rm-latex-automode)
       (find-file (format "%s/%s.tex"
                          (get-zk-tex-dir-path)
                          ns))
